2eb34276b9a5ddb30f4b0c79b6577ce8eba0a397
rafen <capdevielle@gmail.com>
2016-12-16 18:50:37 -0300

    Added http error pages and robots.txt views and templates

--- logicaloutcomes/settings/dev.py
+++ logicaloutcomes/settings/dev.py
@@ -4,3 +4,5 @@ from logicaloutcomes.settings.common import *
 INSTALLED_APPS += ('debug_toolbar', )
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
 INTERNAL_IPS = '127.0.0.1'
+
+DEBUG = True
