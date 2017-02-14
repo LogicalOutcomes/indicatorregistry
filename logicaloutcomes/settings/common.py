0f8d3553a7743dd1c9f1c898c601f4cbad7f405f
rafen <capdevielle@gmail.com>
2017-02-14 12:41:54 -0300

    Updated aristotle-theme app

--- logicaloutcomes/settings/common.py
+++ logicaloutcomes/settings/common.py
@@ -136,12 +136,6 @@ STATIC_PRECOMPILER_COMPILERS = (
     })
 )
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'aristotle_themes.finders.NpmFinder',
-]
-
 ARISTOTLE_THEMES_SCSS = 'scss/indicatorregistry_theme.scss'
 
 ARISTOTLE_THEMES_NAME = 'bootstrap-material-design'
