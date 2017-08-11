28204fa123b4f97852e2b6a7882434ff1efd0081
rafen <capdevielle@gmail.com>
2017-08-11 10:01:49 -0300

    Added bulk download for PDF Summary export

--- local/downloader.py
+++ local/downloader.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from aristotle_mdr.downloader import render_to_pdf, items_for_bulk_download
 from django.conf import settings
-from aristotle_mdr.downloader import render_to_pdf
+from django.utils.safestring import mark_safe
 
 
 item_register = {
@@ -25,7 +25,39 @@ def download(request, download_type, item):
 
 
 def bulk_download(request, download_type, items, title=None, subtitle=None):
-    res = "Not implemented yet: "
-    for item in items:
-        res += ', {}'.format(item)
-    return HttpResponse(res, content_type="text/plain")
+    """Built in download method"""
+    template = 'local/downloads/pdf/bulk_download.html'  # %(download_type)
+    page_size = getattr(settings, 'PDF_PAGE_SIZE', "A4")
+
+    item_querysets = items_for_bulk_download(items, request)
+
+    if title is None:
+        if request.GET.get('title', None):
+            title = request.GET.get('title')
+        else:
+            title = "Auto-generated document"
+
+    if subtitle is None:
+        if request.GET.get('subtitle', None):
+            subtitle = request.GET.get('subtitle')
+        else:
+            _list = "<li>" + "</li><li>".join([item.name for item in items if item]) + "</li>"
+            subtitle = mark_safe("Generated from the following metadata items:<ul>%s<ul>" % _list)
+
+    if download_type == "pdf":
+        debug_as_html = bool(request.GET.get('html', ''))
+
+        return render_to_pdf(
+            template,
+            {
+                'title': title,
+                'subtitle': subtitle,
+                'items': items,
+                'included_items': sorted(
+                    [(k, v) for k, v in item_querysets.items()],
+                    key=lambda k_v: k_v[0]._meta.model_name
+                ),
+                'pagesize': request.GET.get('pagesize', page_size),
+            },
+            debug_as_html=debug_as_html
+        )
